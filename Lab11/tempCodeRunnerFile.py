cbaae0c559d36cb382c09ce843921c32a7f58ab1
K1BOch <mega.sayty@mail.ru>
2023-05-03 22:19:18 +0600

    added lab11

---
+++ Lab11/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+names = ["123", "123", "123"]
+# phones = ["87076039179", "7777777777777777777777777", "7777777777777777777777777"]
+# incorrect_phones = insert_users(5,names, phones)
+# print("Incorrect phones:", incorrect_phones)
+# insert_or_update_user(4,'Kibab', '87076039456')
+# return_all_record()
+# delete_user_by_username_or_phone("Kibab")
